023e0a194f3dc6db6a071965937ef9f186497865
difbit <timo.kekalainen@hotmail.com>
2020-06-25 12:43:58 +0300

    Bar movements and a ball added

--- pong/pong.py
+++ pong/pong.py
@@ -5,7 +5,7 @@ import math
 
 pygame.init()
 
-FPS = 70
+FPS = 60
 fps_clock = pygame.time.Clock()
 
 DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, 32)
@@ -13,10 +13,14 @@ pygame.mouse.set_visible(True)
 
 GRAY  = (128, 128, 128)
 BLACK = (  0,   0,   0)
+SPEED = 25
+BALL_SPEED = 300
 
 def main():
-    bar_one = bar(1800, 40, 10)
-    bar_two = bar(100, 40, 10)
+    elapsed = 0.
+    bar_one = Bar(1800, 40, SPEED)
+    bar_two = Bar(100, 40, SPEED)
+    ball = Ball(400, 300, BALL_SPEED, 0, 0, 30)
     while True:
         DISPLAYSURF.fill(BLACK)
 
@@ -29,14 +33,18 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+        elapsed = fps_clock.tick(FPS)
+        sec = elapsed / 1000.0
+        ball.draw()
+        ball.play(sec)
+        bar_one.play()
         bar_two.move()
         bar_one.draw()
         bar_two.draw()
         pygame.display.update()
-        fps_clock.tick(FPS)
 
 
-class bar(object):
+class Bar(object):
 
     def __init__(self, x, y, v):
         self.x = x
@@ -51,9 +59,50 @@ class bar(object):
     def move(self):
         self.y += self.v
         if self.y <= 0:
-            self.v = 10
-        if self.y >= 950:
-            self.v = -10
+            self.v = SPEED
+        elif self.y >= 950:
+            self.v = -SPEED
+
+    def play(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.y >= 10:
+            self.y -= SPEED
+        elif keys[pygame.K_DOWN] and self.y <= 940:
+            self.y += SPEED
+
+
+class Ball(object):
+
+    def __init__(self, x, y, v, vx, vy, THETA):
+        self.x = x
+        self.y = y
+        self.v = v
+        self.THETA = THETA
+
+    def draw(self):
+        pygame.draw.rect(
+            DISPLAYSURF, GRAY, ((self.x, self.y, 15, 15))
+        )
+
+    def play(self, sec):
+        self.vx = self.v * math.cos(self.THETA * math.pi/180)
+        self.vy = self.v * math.sin(self.THETA * math.pi/180)
+        self.x = self.x + self.vx * sec
+#         self.x = 0.001 * sec
+        self.y = self.y + self.vy * sec
+#         self.y = 0.001 * sec
+#         if self.x >
+
+        if self.y <= 0:
+            self.vy = self.vy
+        elif self.y >= 950:
+            self.vy = -self.vy
+
+        if self.x <= 5:
+            self.vx = self.vx
+        elif self.x >= 950:
+            self.vx = -self.vx
+
 
 
 if __name__=='__main__':
