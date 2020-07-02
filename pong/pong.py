8dcf884382f8335c90f4f86cbb912524bfc3076b
difbit <timo.kekalainen@hotmail.com>
2020-07-02 12:48:26 +0300

    Debug mode added and fixed the problem of ball bouncing from sidewalls

--- pong/pong.py
+++ pong/pong.py
@@ -5,26 +5,26 @@ import math
 
 pygame.init()
 
-FPS = 60
+FPS = 30
 fps_clock = pygame.time.Clock()
 
 DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, 32)
 pygame.mouse.set_visible(False)
 
+DEBUG_MODE = False
 GRAY  = (128, 128, 128)
 BLACK = (  0,   0,   0)
 SPEED = 25
 BALL_SPEED = 300
 BALL_ANGLE = 155
 
-
 def main():
     SCORE_ONE = 0
     SCORE_TWO = 0
     elapsed = 0.
-    bar_one = Bar(1800, 40, SPEED)
+    bar_one = Bar(DISPLAYSURF.get_width() - 100, 540, SPEED)
     bar_two = Bar(100, 540, SPEED)
-    ball = Ball(1200, 800, BALL_SPEED, 0, 0, BALL_ANGLE)
+    ball = Ball(DISPLAYSURF.get_width() / 2, 800, BALL_SPEED, 0, 0, BALL_ANGLE)
     while True:
         DISPLAYSURF.fill(BLACK)
 
@@ -40,7 +40,7 @@ def main():
         FONT_SCORE = pygame.font.Font('freesansbold.ttf', 45)
         score_surface = FONT_SCORE.render(
             '%r   %r' % (SCORE_ONE, SCORE_TWO), True, GRAY)
-        DISPLAYSURF.blit(score_surface, (900, 25))
+        DISPLAYSURF.blit(score_surface, (DISPLAYSURF.get_width() / 2 - 55, 25))
 
         elapsed = fps_clock.tick(FPS)
         sec = elapsed / 1000.0
@@ -49,15 +49,32 @@ def main():
         bar_two.move()
 
         diff_angle = 90 - ball.THETA
-        if (abs(bar_one.x - ball.x) < 7 and
-                (bar_one.y - ball.y) > -130):
+        if DEBUG_MODE:
+            COORDS = pygame.font.Font('freesansbold.ttf', 40)
+            bar_two_x = COORDS.render(
+                'bar_two.x: %r' % (bar_two.x), True, GRAY)
+            DISPLAYSURF.blit(bar_two_x, (500, 85))
+            ball_x = COORDS.render(
+                'ball.x: %r' % (ball.x), True, GRAY)
+            DISPLAYSURF.blit(ball_x, (500, 125))
+            bar_two_y = COORDS.render(
+                'bar_two.y: %r' % (bar_two.y), True, GRAY)
+            DISPLAYSURF.blit(bar_two_y, (500, 165))
+            ball_y = COORDS.render(
+                'ball.y: %r' % (ball.y), True, GRAY)
+            DISPLAYSURF.blit(ball_y, (500, 205))
+            ball_diff_y = COORDS.render(
+                'bar_two.y - ball.y: %r' % (ball.y - bar_two.y), True, GRAY)
+            DISPLAYSURF.blit(ball_diff_y, (500, 245))
+
+        if (abs(bar_one.x - ball.x) < 14 and
+                0 < (ball.y - bar_one.y) < 130):
             if ball.vy < 0:
                 ball.THETA += 2 * diff_angle
             elif ball.vy > 0:
                 ball.THETA += 2 * diff_angle
-
-        if (abs(bar_two.x - ball.x) < 7 and
-                (bar_two.y - ball.y) > -130):
+        if (abs(bar_two.x - ball.x) < 14 and
+                0 < (ball.y - bar_two.y) < 130):
             if ball.vy < 0:
                 ball.THETA += 2 * diff_angle
             elif ball.vy > 0:
@@ -66,10 +83,10 @@ def main():
         bar_one.draw()
         bar_two.draw()
         if ball.x < 10:
-            ball.x, ball.y = 1200, 800
+            ball.x, ball.y = DISPLAYSURF.get_width() / 2, 800
             SCORE_TWO += 1
-        if ball.x > 1900:
-            ball.x, ball.y = 1200, 800
+        if ball.x > DISPLAYSURF.get_width():
+            ball.x, ball.y = DISPLAYSURF.get_width() / 2, 800
             SCORE_ONE += 1
         ball.draw()
         pygame.display.update()
